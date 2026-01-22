cbffa84380469183b09b34570ca8f82256e0761a
Onyechevinli <umeokolivincent@gmail.com>
2026-01-22 22:54:22 +0100

    update backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "backend-rg"
-    storage_account_name = "clearworktfbackend"
+    storage_account_name = "clearworktfbackend1"
     container_name       = "tfstate"
     key                  = "dev/terraform.tfstate"
   }
